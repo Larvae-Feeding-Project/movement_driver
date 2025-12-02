d08596105a3a433a085a93c13076fb46315914f2
nitai.gildor <nitai.gildor@mail.huji.ac.il>
2025-12-02 19:41:41 +0200

    Mostly implemented functions, need to address return vals. updated readme

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # movement_driver
-This repository houses our movement driver along with a few utils that use it
+
+This repo is used to control the movement system. 
+It includes the movement system driver, its local data, and a few 
+examples and utils that can be used for calibration.
+
+
+## Dependencies
+
+-pySerial
+
+
+
+## Usage
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first
+to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
--- movement_driver.py
+++ movement_driver.py
@@ -3,7 +3,6 @@ import serial
 import json
 
 
-
 class MovementDriver:
 
     def __init__(self):
@@ -25,8 +24,46 @@ class MovementDriver:
         except Exception as e:
             print("Movement system connection error")
 
+        self.reset()
+
     def __del__(self):
+        """
+        Movement system destructor. Resets the system and closes the serial port
+        :return:
+        """
         # Close movement serial
+        self.reset()
         self.movement_ser.close()
 
-    #def reset(self):
+    def reset(self):
+        """
+        Resets the movement system, which means moving it to (0,0,0) and resetting relative location
+        :return:
+        """
+        self._send_command("G28")
+
+    """Need to implement the string refactoring"""
+    def move(self, x='', y='', z='', speed=3000):
+        self._send_command("G0 X100 Y150 Z50 F3000")
+
+    """
+    Need to check how to receive and present the position
+    """
+    def get_position(self):
+        self._send_command("M114")
+
+    """
+    Need to analyze how to loop responses are built
+    """
+    def _send_command(self, command):
+        # Encode and send the command to the movement system
+        self.movement_ser.write((command + "\n").encode())
+        self.movement_ser.flush()
+
+        # Read and print all responses until an "ok" or empty line
+        while True:
+            resp = self.movement_ser.readline().decode("ascii", errors="ignore").strip()
+            if resp:
+                print("<<", resp)
+            if resp == "ok" or resp == "":
+                break
