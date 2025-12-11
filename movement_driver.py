bcb6d700226cc26f0641ef47144eec414c7d7f77
Nitai-Gildor <nitai.gildor@mail.huji.ac.il>
2025-12-11 23:13:41 +0200

    implemented move function with limit checks

--- movement_driver.py
+++ movement_driver.py
@@ -7,7 +7,7 @@ class MovementDriver:
 
     def __init__(self):
 
-        # Open relevant printer data
+        # Open printer data dict
         try:
             with open('data.json', 'r') as file:
                 self.movement_data = json.load(file)
@@ -15,6 +15,10 @@ class MovementDriver:
             print('No movement data file found')
         except Exception as e:
             print("Exception occurred")
+
+        # Load movement system bounds
+        self.XLIMIT, self.YLIMIT, self.ZLIMIT = \
+            self.movement_data['XLIMIT'], self.movement_data['YLIMIT'], self.movement_data['ZLIMIT']
 
         # Open serial with the movement system
         try:
@@ -24,6 +28,7 @@ class MovementDriver:
         except Exception as e:
             print("Movement system connection error")
 
+        # Reset system
         self.reset()
 
     def __del__(self):
@@ -38,13 +43,44 @@ class MovementDriver:
     def reset(self):
         """
         Resets the movement system, which means moving it to (0,0,0) and resetting relative location
-        :return:
+        :return: VOID
         """
         self._send_command("G28")
 
-    """Need to implement the string refactoring"""
-    def move(self, x='', y='', z='', speed=3000):
-        self._send_command("G0 X100 Y150 Z50 F3000")
+    def move(self, x=None, y=None, z=None, speed=3000):
+        """
+        Move to specific location. If an axis is not specified, the location on that axis will not change
+        :param x: x coordinate (float)
+        :param y: y coordinate (float)
+        :param z: z coordinate (float)
+        :param speed: speed of movement (float)
+        :return: VOID
+        """
+        # Validate coordinates only if provided
+        if x is not None:
+            if x < 0 or x > self.XLIMIT:
+                raise Exception("Invalid X movement command")
+
+        if y is not None:
+            if y < 0 or y > self.YLIMIT:
+                raise Exception("Invalid Y movement command")
+
+        if z is not None:
+            if z < 0 or z > self.ZLIMIT:
+                raise Exception("Invalid Z movement command")
+
+        # Movement command list
+        parts = ["G0"]
+
+        if x is not None:
+            parts.append(f"X{x}")
+        if y is not None:
+            parts.append(f"Y{y}")
+        if z is not None:
+            parts.append(f"Z{z}")
+
+        parts.append(f"F{speed}")
+        self._send_command(" ".join(parts))
 
     """
     Need to check how to receive and present the position
